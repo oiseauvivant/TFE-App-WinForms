ce4c248f4ff92c3beac8324a8540ca500af256e1
oiseauvivant <thomish2004@gmail.com>
2026-04-13 11:05:36 +0200

    Ajoutez des fichiers projet.

---
+++ Test_TFE/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Test_TFE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnEnvoi = new System.Windows.Forms.Button();
+            this.txtEnvoi = new System.Windows.Forms.TextBox();
+            this.lblsortie = new System.Windows.Forms.Label();
+            this.lblentree = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.pnlId1 = new System.Windows.Forms.Panel();
+            this.pnlId2 = new System.Windows.Forms.Panel();
+            this.tmrClignoPorte = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnEnvoi
+            // 
+            this.btnEnvoi.Location = new System.Drawing.Point(138, 281);
+            this.btnEnvoi.Name = "btnEnvoi";
+            this.btnEnvoi.Size = new System.Drawing.Size(75, 23);
+            this.btnEnvoi.TabIndex = 0;
+            this.btnEnvoi.Text = "Envoyer";
+            this.btnEnvoi.UseVisualStyleBackColor = true;
+            this.btnEnvoi.Click += new System.EventHandler(this.envoyer);
+            // 
+            // txtEnvoi
+            // 
+            this.txtEnvoi.Location = new System.Drawing.Point(98, 77);
+            this.txtEnvoi.Multiline = true;
+            this.txtEnvoi.Name = "txtEnvoi";
+            this.txtEnvoi.Size = new System.Drawing.Size(158, 131);
+            this.txtEnvoi.TabIndex = 1;
+            // 
+            // lblsortie
+            // 
+            this.lblsortie.AutoSize = true;
+            this.lblsortie.Location = new System.Drawing.Point(457, 158);
+            this.lblsortie.Name = "lblsortie";
+            this.lblsortie.Size = new System.Drawing.Size(38, 13);
+            this.lblsortie.TabIndex = 2;
+            this.lblsortie.Text = "sortie :";
+            // 
+            // lblentree
+            // 
+            this.lblentree.AutoSize = true;
+            this.lblentree.Location = new System.Drawing.Point(457, 195);
+            this.lblentree.Name = "lblentree";
+            this.lblentree.Size = new System.Drawing.Size(43, 13);
+            this.lblentree.TabIndex = 3;
+            this.lblentree.Text = "entree :";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(446, 250);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(37, 13);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "Total :";
+            // 
+            // pnlId1
+            // 
+            this.pnlId1.BackColor = System.Drawing.Color.Green;
+            this.pnlId1.Location = new System.Drawing.Point(549, 71);
+            this.pnlId1.Name = "pnlId1";
+            this.pnlId1.Size = new System.Drawing.Size(81, 56);
+            this.pnlId1.TabIndex = 5;
+            // 
+            // pnlId2
+            // 
+            this.pnlId2.BackColor = System.Drawing.Color.Green;
+            this.pnlId2.Location = new System.Drawing.Point(549, 148);
+            this.pnlId2.Name = "pnlId2";
+            this.pnlId2.Size = new System.Drawing.Size(95, 69);
+            this.pnlId2.TabIndex = 6;
+            // 
+            // tmrClignoPorte
+            // 
+            this.tmrClignoPorte.Interval = 200;
+            this.tmrClignoPorte.Tick += new System.EventHandler(this.clignoPorte);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 513);
+            this.Controls.Add(this.pnlId2);
+            this.Controls.Add(this.pnlId1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblentree);
+            this.Controls.Add(this.lblsortie);
+            this.Controls.Add(this.txtEnvoi);
+            this.Controls.Add(this.btnEnvoi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnvoi;
+        private System.Windows.Forms.TextBox txtEnvoi;
+        private System.Windows.Forms.Label lblsortie;
+        private System.Windows.Forms.Label lblentree;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Panel pnlId1;
+        private System.Windows.Forms.Panel pnlId2;
+        private System.Windows.Forms.Timer tmrClignoPorte;
+    }
+}
+
---
+++ Test_TFE/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Test_TFE
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        string[] parties;
+        string id;
+        string sens;
+        string nbr;
+        int total = 0;
+        int entree = 0;
+        int sortie = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //id=1;sens=entree;nbr=1
+
+
+            serialPort = new SerialPort("COM5", 115200, Parity.None, 8, StopBits.One);
+            serialPort.DataReceived += recevoir;
+            try
+            {
+                serialPort.Open();
+                //MessageBox.Show("Port série ouvert !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur ouverture port : " + ex.Message);
+            }
+            
+
+        }
+
+        private void envoyer(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                string message = txtEnvoi.Text; // Récupère le contenu du TextBox
+                serialPort.WriteLine(message);  // Envoie au port série
+            }
+            else
+            {
+                MessageBox.Show("Port série non ouvert !");
+            }
+        }
+
+        private void recevoir(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                string messageRecu = serialPort.ReadLine(); // Lit la ligne reçue
+                this.Invoke(new Action(() =>
+                {
+                    txtEnvoi.Text = messageRecu; // Affiche le message reçu dans le TextBox
+                    parties = messageRecu.Split(';');
+
+                    id = parties[0].Split('=')[1];
+                    sens = parties[1].Split('=')[1];
+                    nbr = parties[2].Split('=')[1];
+
+                    if (sens == "entree")
+                    {
+                        total++;
+                        entree++;
+                        lblentree.Text = "Entrées: " + entree.ToString();
+                    }
+                    else if (sens == "sortie")
+                    {
+                        total--;
+                        sortie++;
+                        lblsortie.Text = "Sorties: " + sortie.ToString();
+                    }
+
+                    lblTotal.Text = "Total: " + total.ToString();
+
+                    if (id == "1")
+                    {
+                        pnlId1.BackColor = Color.Red;
+                    }
+                    else if (id == "2")
+                    {
+                        pnlId2.BackColor = Color.Red; 
+                    }
+
+                    tmrClignoPorte.Start();
+                }));
+
+            }
+
+            
+        }
+
+        private void clignoPorte(object sender, EventArgs e)
+        {
+            pnlId1.BackColor = Color.Green;
+            pnlId2.BackColor = Color.Green;
+            tmrClignoPorte.Stop();
+        }
+    }
+}
